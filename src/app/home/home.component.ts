e3874ae80e4fe0bca6c9a0fe617baf90fa4db8aa
Fred Morrison <frederick.morrison@outlook.com>
2024-12-04 20:32:33 -0700

    generate boilerplate Housing Location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import {CommonModule} from '@angular/common';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+//
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
