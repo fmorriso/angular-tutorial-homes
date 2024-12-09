94da018528584a63d34199a53f241ef3368bd68d
Fred Morrison <frederick.morrison@outlook.com>
2024-12-09 11:10:23 -0700

    add @angular/material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 //
 import { HomeComponent } from './home/home.component';
 import {DetailsComponent} from './details/details.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 export const routeConfig: Routes = [
-  { path: '', component: HomeComponent, title:'Home Page' },
-  {path: 'details/:id', component: DetailsComponent, title: 'Details Page' },
+  { path: 'home', component: HomeComponent, title:'Home Page' },
+  { path: 'details/:id', component: DetailsComponent, title: 'Details Page' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatChipsModule} from '@angular/material/chips';
+
+@Component({
+  selector: 'app-page-not-found',
+  imports: [MatChipsModule],
+  templateUrl: './page-not-found.component.html',
+  styleUrl: './page-not-found.component.scss'
+})
+export class PageNotFoundComponent {
+
+}
